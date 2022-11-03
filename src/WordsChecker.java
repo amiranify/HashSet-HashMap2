1878e74cd0ba53e6d91d7c824d3eee48c7df4910
amiranify <amiranify@mail.ru>
2022-11-03 20:32:22 +0300

    reformat code

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -4,12 +4,13 @@ import java.util.Set;
 
 public class WordsChecker {
     protected String text;
-
     Set<String> words;
-    public WordsChecker(String text){
+
+    public WordsChecker(String text) {
         words = new HashSet<>(List.of(text.split("\\P{IsAlphabetic}+")));
     }
-    public boolean hasWord(String word){
+
+    public boolean hasWord(String word) {
         return words.contains(word);
     }
 }
